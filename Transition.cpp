9daa6dfe7329949cf04dda0a75bd467d8751efaa
Slash <123@123>
2023-06-30 12:04:16 +0200

    final

--- Transition.cpp
+++ Transition.cpp
@@ -333,15 +333,13 @@ void Transition::initRotationY()
     vertices1.push_back(QVector4D(1,-1,0,1)); //C
     vertices1.push_back(QVector4D(1,1,0,1)); //D
 
-    vertices2.push_back(QVector4D(-1,-1,0,1)); //B
-    vertices2.push_back(QVector4D(-1,1,0,1)); //A
     vertices2.push_back(QVector4D(1,1,0,1)); //D
     vertices2.push_back(QVector4D(1,-1,0,1)); //C
-
+    vertices2.push_back(QVector4D(-1,-1,0,1)); //B
+    vertices2.push_back(QVector4D(-1,1,0,1)); //A
 
     triangles.push_back(QVector3D(2,1,0)); //CBA
     triangles.push_back(QVector3D(2,0,3)); //CAD
-
 
     UVvertices.push_back(QVector2D(0,0)); //A
     UVvertices.push_back(QVector2D(0,1)); //B
